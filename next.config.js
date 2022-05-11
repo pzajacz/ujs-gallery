e0afda8745ba27a40a3339c404231921d2660c59
pzajacz <pzajacz@brainsum.com>
2022-05-11 11:19:59 +0200

    UJSG-003 | Add PWA.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
+const withPlugins = require('next-compose-plugins');
+const withPWA = require("next-pwa");
+
+module.exports = withPlugins([
+  // another plugin with a configuration
+  [withPWA, {
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+      disable: process.env.NODE_ENV === "development",
+    },
+  }],
+
+], {
   reactStrictMode: true,
   images: {
     domains: ['ujszo.com'],
     formats: ['image/webp'],
   },
-}
+});
 
-module.exports = nextConfig
